adeb03fca2fbdc13b3d4e7113311d3f1a3c5b3d6
bikerornot <james@bikerornot.com>
2026-04-23 15:39:33 -0400

    Check-in: simpler place join + composer debug log

--- src/app/components/PostComposer.tsx
+++ src/app/components/PostComposer.tsx
@@ -182,10 +182,13 @@ export default function PostComposer({ currentUserProfile, wallOwnerId, wallOwne
       if (checkedInPlace) {
         try {
           const placeId = await getOrCreatePlace(checkedInPlace)
+          console.log('[check-in] resolved place', { placeId, name: checkedInPlace.name })
           formData.set('placeId', placeId)
         } catch (err) {
-          console.warn('Could not resolve place for check-in', err)
+          console.warn('[check-in] Could not resolve place', err)
         }
+      } else {
+        console.log('[check-in] no checkedInPlace at submit')
       }
       images.forEach((file) => formData.append('images', file))
 
